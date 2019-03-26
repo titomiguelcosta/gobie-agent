4c50bb8e53ca9b5a2f3529a87ac694439f5b54da
Tito Costa <tito.costa@mightyape.co.nz>
2019-03-26 17:02:55 +1300

    integrated psysh and allow docker application to run one image

--- src/Application/Docker.php
+++ src/Application/Docker.php
@@ -22,15 +22,23 @@ final class Docker implements ApplicationInterface
     /** @var bool */
     private $isSupported = false;
 
+    /**
+     * @var Process
+     */
+    private $process = null;
+
+    /** @var array */
+    private $instances = [];
+
     public function __construct()
     {
-        $process = new Process('docker --version');
+        $process = new Process(['docker', '--version']);
         $process->run();
 
         if ($process->isSuccessful()) {
             $this->isInstalled = true;
         }
-        
+
         $dockerVersionParser = new DockerVersionParser($process->getOutput());
         $this->version = $dockerVersionParser->getVersion();
         $this->build = $dockerVersionParser->getBuild();
@@ -38,12 +46,12 @@ final class Docker implements ApplicationInterface
         if (Comparator::greaterThanOrEqualTo($dockerVersionParser->getVersion(), self::MINIMUM_VERSION)) {
             $this->isSupported = true;
         }
-    }   
+    }
 
     /**
      * @return null|string
      */
-    public function getVersion(): ?string
+    public function getVersion(): ? string
     {
         return $this->version;
     }
@@ -51,11 +59,11 @@ final class Docker implements ApplicationInterface
     /**
      * @return null|string
      */
-    public function getBuild(): ?string
+    public function getBuild(): ? string
     {
         return $this->build;
     }
-    
+
     /**
      * @return bool
      */
@@ -63,12 +71,45 @@ final class Docker implements ApplicationInterface
     {
         return $this->isInstalled;
     }
-    
+
     /**
      * @return bool
      */
     public function isSupported(): bool
     {
         return $this->isSupported;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRunning(): bool
+    {
+        return $this->process instanceof Process && $this->process->isRunning();
+    }
+
+    /**
+     * @return Process|null
+     */
+    public function getProcess(): ? Process
+    {
+        return $this->process;
+    }
+
+    /**
+     * @param string $image
+     * @param string $name
+     * @param array $options
+     * @return Process
+     */
+    public function run(string $image, ? string $name = null, array $options = []): Process
+    {
+        if (!$this->isRunning()) {
+            $command = ['docker', 'run', '--name', $name, '-t', $image, ];
+            $this->process = new Process(array_merge($command, $options));
+            $this->process->start();
+        }
+
+        return $this->process;
     }
 }
