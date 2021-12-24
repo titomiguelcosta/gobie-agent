b7ea3f5ae677031168916b8b71ad694ebed9b6f0
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-12-24 13:26:26 +0100

    upgraded to php 8.1 and symfony 6

--- Makefile
+++ Makefile
@@ -10,14 +10,16 @@ docker-build:
 	docker build -t titomiguelcosta/gobie-php73 -f docker/gobie-php73.dockerfile .
 	docker build -t titomiguelcosta/gobie-php74 -f docker/gobie-php74.dockerfile .
 	docker build -t titomiguelcosta/gobie-php80 -f docker/gobie-php80.dockerfile .
+	docker build -t titomiguelcosta/gobie-php81 -f docker/gobie-php81.dockerfile .
 
 docker-push:
 	docker push titomiguelcosta/gobie-php73
 	docker push titomiguelcosta/gobie-php74
 	docker push titomiguelcosta/gobie-php80
+	docker push titomiguelcosta/gobie-php81
 
 docker-ssh:
-	docker run --entrypoint /bin/bash -it titomiguelcosta/gobie-php74
+	docker run --entrypoint /bin/bash -it titomiguelcosta/gobie-php81
 
 php-cs-fixer:
 	php vendor/bin/php-cs-fixer fix src/
--- config/bundles.php
+++ config/bundles.php
@@ -4,11 +4,9 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
